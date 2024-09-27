c2f838e0a6a77423b4ab5008edc80a73d5ff4411
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-09-27 14:03:40 +0500

    images swip project completer

--- images Swipe/script.js
+++ images Swipe/script.js
@@ -5,3 +5,28 @@
 // }
 
 // this is the second way to do it
+
+const swapImage1 = document.getElementById("swapImage1");
+const swapImage2 = document.getElementById("swapImage2");
+const swapImage3 = document.getElementById("swapImage3");
+
+// for image 1
+swapImage1.addEventListener("click", function () {
+  const swipeImage = document.getElementById("swipeImage");
+  const url = swapImage1.src;
+  swipeImage.src = url;
+});
+
+// for image 2
+swapImage2.addEventListener("click", function () {
+  const swipeImage = document.getElementById("swipeImage");
+  const url = swapImage2.src;
+  swipeImage.src = url;
+});
+
+// for image 3
+swapImage3.addEventListener("click", function () {
+  const swipeImage = document.getElementById("swipeImage");
+  const url = swapImage3.src;
+  swipeImage.src = url;
+});
