ab4136d406895a7ec0b96e120ec604775b55a9ef
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-09-27 13:55:11 +0500

    Project of Swipe Images In Js

---
+++ images Swipe/script.js
@@ -0,0 +1,7 @@
+// You can do like this
+// function swapImage(imgsrc) {
+//   const swipeImage = document.getElementById("swipeImage");
+//   swipeImage.src = imgsrc;
+// }
+
+// this is the second way to do it
