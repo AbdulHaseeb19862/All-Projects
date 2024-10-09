837e8e75743f9f9d35e26f762bfb97b8a7072391
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-10-10 00:39:33 +0500

    In TodoApp Again I create li full requirements

---
+++ JS WORK/TodoApp Again/script.js
@@ -0,0 +1,61 @@
+const addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function () {
+  const todoInput = document.getElementById("todoInput");
+  if (!todoInput.value.trim("")) {
+    return alert("Todo value can't empty!");
+  }
+  const ul = document.getElementById("listItems");
+  // Check if height exceeds 300px
+  if (ul.scrollHeight > 300) {
+    ul.style.overflowY = "auto";
+  }
+  const li = document.createElement("li");
+  li.classList.add("list");
+  const span = document.createElement("span");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.id = "myCheckbox";
+  checkbox.name = "myCheckbox";
+  checkbox.checked = false;
+
+  const div1 = document.createElement("div");
+  div1.classList.add("div1");
+  div1.append(checkbox);
+
+  span.textContent = todoInput.value;
+  div1.append(span);
+
+  li.append(div1);
+
+  const edit = document.createElement("span");
+  edit.textContent = "EDIT";
+  edit.style.color = "green";
+  const complete = document.createElement("span");
+  complete.textContent = "COMPLETE";
+  complete.addEventListener("click", function () {
+    li.firstChild.classList.toggle("toggle");
+  });
+  complete.style.color = "yellow";
+  const delet = document.createElement("span");
+  delet.textContent = "DELETE";
+  delet.style.color = "red";
+  delet.addEventListener("click", function () {
+    if (checkbox.checked === true) {
+      ul.removeChild(li);
+    } else {
+      alert("The checkbox has not been checked");
+    }
+  });
+
+  const div2 = document.createElement("div");
+
+  div2.classList.add("liContainer");
+
+  div2.append(edit);
+  div2.append(complete);
+  div2.append(delet);
+
+  li.append(div2);
+
+  ul.appendChild(li);
+});
