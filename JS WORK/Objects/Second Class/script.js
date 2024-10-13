33fea1102d074df563a6419c0193a7077746cb5e
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-10-13 22:45:38 +0500

    work on object properties

---
+++ JS WORK/Objects/Second Class/script.js
@@ -0,0 +1,57 @@
+// Can we create empty object
+// const empty = {};
+// console.log(empty);
+
+// We can add property and with dot notation and with bracket notation
+
+// const car = {};
+
+// car.name = "Honda Civic"; // using dot notation
+// car.model = "2014";
+// car["owner"] = "Haider Bangash"; // using bracket notation
+// console.log(car);
+
+// Accessing the properties
+
+// const car = {
+//   name: "Honda Civic",
+//   model: "2021",
+//   owner: "Haider Bangash",
+// };
+
+// console.log(car.model);
+
+// We can Edit the value
+
+// const car = {
+//   name: "Honda Civic",
+//   model: "2021",
+//   owner: "Haider Bangash",
+// };
+
+// car.name = "Land Cruser"; // using dot notation
+// car["model"] = "2024";
+
+// console.log(car);
+
+//  How can we delete the properties
+
+// const car = {
+//   name: "Honda Civic",
+//   model: "2021",
+//   owner: "Haider Bangash",
+// };
+
+// delete car.owner;
+// console.log(car);
+
+// Check in existence
+
+// const car = {
+//   name: "Honda Civic",
+//   model: "2021",
+//   owner: "Haider Bangash",
+// };
+
+// console.log("owner" in car); // Which Have Existence
+// console.log("tyres" in car); // which does not have existence
