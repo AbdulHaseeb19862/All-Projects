f30d1e7609f168dd822c238f0fa7ad2fb587e719
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-10-17 16:14:34 +0500

    Work on browser control

---
+++ JS WORK/Browser Control/Getting and Setting URL/script.js
@@ -0,0 +1,21 @@
+// Getting URL  : This shows the complete web address.
+// const url = window.location.href;
+// console.log(url);
+
+// Getting HostName
+// const hostName = window.location.hostname;
+// console.log(hostName)
+
+// Getting Path
+// const path = window.location.pathname;
+// console.log(path);
+
+//  Getting Hash/ Anchor  (#services)
+
+// var hash = window.location.hash;
+// console.log(hash);
+
+// How can set the URL
+
+var settingURL = (window.location.href = "https:www.google.com");
+console.log(settingURL);
