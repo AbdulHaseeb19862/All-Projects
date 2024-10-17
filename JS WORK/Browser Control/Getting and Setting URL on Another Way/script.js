e0ec6f376cebbe68d207ecd7a5bd7b0153a3323d
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-10-17 18:15:09 +0500

    In Browser Chapter 77 complete

---
+++ JS WORK/Browser Control/Getting and Setting URL on Another Way/script.js
@@ -0,0 +1,11 @@
+// This is the another way we change the URL of particular website
+
+//1.  Assign method // this method can store the history
+// window.location.assign("http://www.google.com");
+
+// 2. Using window.location.replace() this method does not store the history
+
+// 3. Reloading The Current Page
+window.location.reload(true); // Reload from server
+// window.location.reload(false); //Reload from cache
+// window.location(); // Default: Cache
