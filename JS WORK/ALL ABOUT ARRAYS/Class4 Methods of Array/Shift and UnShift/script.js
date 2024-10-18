b24822f3e2a149739b6460c56769d2557490f8f8
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-10-19 01:14:30 +0500

    Work on shift and unshift method in javascript

---
+++ JS WORK/ALL ABOUT ARRAYS/Class4 Methods of Array/Shift and UnShift/script.js
@@ -0,0 +1,12 @@
+// shift():
+// The shift() method removes the first element from an array and returns it. The remaining elements are shifted to a lower index.
+
+// unshift():
+// The unshift() method adds one or more elements to the beginning of an array and returns the new length of the array.
+
+let arr = ["Rahool", "Mohib", "Fayaz", "Haseeb Bangash"];
+document.write(arr + "<br>");
+arr.shift();
+document.write(arr + "<br>");
+arr.unshift("Abdul Haseeb Bangash");
+document.write(arr + "<br>");
