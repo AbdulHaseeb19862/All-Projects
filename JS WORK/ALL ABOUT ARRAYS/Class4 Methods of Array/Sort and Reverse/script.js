8befd68f6d670c851191598edda234f6d5e8efb3
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-10-19 00:35:04 +0500

    methods of array sort and reverse

---
+++ JS WORK/ALL ABOUT ARRAYS/Class4 Methods of Array/Sort and Reverse/script.js
@@ -0,0 +1,12 @@
+// sort():
+// The sort() method arranges the elements of an array in alphabetical or numerical order. By default, it sorts elements as strings.
+
+const arr1 = [2, 1, 4, 3, 5];
+document.write(arr1 + "<br><br>");
+arr1.sort();
+document.write(arr1 + "<br>");
+
+// reverse():
+// The reverse() method reverses the order of the elements in an array.
+arr1.reverse();
+document.write(arr1 + "<br>");
