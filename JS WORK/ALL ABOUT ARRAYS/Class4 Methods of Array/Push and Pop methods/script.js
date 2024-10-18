73464a03d32e385c0af13f88ad8db0804ca0e9f5
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-10-19 00:51:15 +0500

    work on pop and push method of array

---
+++ JS WORK/ALL ABOUT ARRAYS/Class4 Methods of Array/Push and Pop methods/script.js
@@ -0,0 +1,13 @@
+// pop():
+// The pop() method removes the last element from an array and returns that element.
+
+// push():
+// The push() method adds one or more elements to the end of an array and returns the new length of the array.
+
+let arr = ["Haseeb", "Jalil", "Fayaz"];
+document.write(arr + "<br>");
+arr.pop();
+document.write(arr + "<br>");
+
+arr.push("Fayaz Khan");
+document.write(arr);
