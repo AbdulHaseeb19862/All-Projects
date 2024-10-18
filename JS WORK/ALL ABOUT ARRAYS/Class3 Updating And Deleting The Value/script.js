3a29856a39ce2df345535fddfbf330a9d6086619
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-10-19 00:11:07 +0500

    Work array update and delete

---
+++ JS WORK/ALL ABOUT ARRAYS/Class3 Updating And Deleting The Value/script.js
@@ -0,0 +1,10 @@
+// const arr1 = ["Hello", 23, "World"];
+// document.write(arr1 + "<br>");
+// // Update here the value
+// arr1[1] = 24;
+// document.write(arr1);
+
+// Deleting Values
+const arr1 = ["Hello", 23, "World"];
+delete arr1[1];
+document.write(arr1);
