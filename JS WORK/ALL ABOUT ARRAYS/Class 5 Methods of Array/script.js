ba98561e97e8c87175b1799513ddfd4d0f66eec6
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-11-03 02:57:59 +0500

    Work on indexOf, lastIndexOf and includes methods

---
+++ JS WORK/ALL ABOUT ARRAYS/Class 5 Methods of Array/script.js
@@ -0,0 +1,22 @@
+// includes() : The includes() method is used to check if an array contains a specific value. It returns true if the value is found in the array, and false if it’s not.
+
+// indexOf(): Finds the first occurrence of a given value in an array, starting from the beginning. It returns the index of that occurrence or -1 if not found.
+
+// lastIndexOf(): Finds the last occurrence of a given value in an array, starting from the end. It returns the index of that occurrence or -1 if not found.
+
+// var a = ["Haseeb", "Jalil", "Haseeb", "Fayaz", "Haider"];
+// document.write(a, "<br><br>");
+
+// using here indexOf Method
+// var b = a.indexOf("Haseeb", 1);
+// document.write(b, "<br><br>");
+
+//using here lastIndexOf Method
+// var b = a.lastIndexOf("Jalil", -2);
+// document.write(b, "<br><br>");
+
+// using include method here
+
+// let fruits = ["apple", "banana", "cherry"];
+// var b = fruits.includes("banana");
+// document.write(b, "<br><br>");
