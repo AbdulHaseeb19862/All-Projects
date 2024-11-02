240c37f9bf863b152c23b29db97db4eb145c6977
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-11-03 02:45:14 +0500

    Prictic on all methods

---
+++ JS WORK/ALL ABOUT ARRAYS/Class4 Methods of Array/Prictice On All Function/script.js
@@ -0,0 +1,44 @@
+// const arr1 = new Array(3);
+// for (var i = 0; i < arr1.length; i++) {
+//   var value = prompt("Enter Value Here");
+//   document.write(`The Value You Have Entered ${value}`);
+//   document.write("<br>");
+// }
+
+// alert("The Array length is completed here");
+
+// fill Methods
+// const arr1 = new Array(4);
+// console.log(arr1.fill(0));
+
+// pop and push method
+
+// const arr1 = ["apple", "Orange", "Banana"];
+// arr1.push("Water Melon");
+// console.log(arr1);
+
+// const arr1 = ["apple", "Orange", "Banana"];
+// arr1.pop();
+// console.log(arr1);
+
+// concat and join method
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+// const arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+// const arr1 = ["Apple", "Orange", "Banana"];
+// const result = arr1.join(",");
+// console.log(result);
+
+// slice and splice method
+// let fruits = ["apple", "banana", "cherry", "date"];
+// let result = fruits.slice(1, 3);
+// console.log(result);
+
+let fruits = ["apple", "banana", "cherry", "date"];
+// fruits.splice(1, 2);
+// console.log(fruits);
+
+// fruits.splice(1, 1, "Water Melon");
+// console.log(fruits);
